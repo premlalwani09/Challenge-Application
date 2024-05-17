0ec6640fc83d6eda0789d033ee13591c1a575a56
Prem Lalwani <premprakashlalwani@gmail.com>
2024-05-18 01:20:23 +0530

    Readme file uploaded

--- src/main/java/com/app/ChallengeApp/ChallengeController.java
+++ src/main/java/com/app/ChallengeApp/ChallengeController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/challenges")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ChallengeController {
 
     private ChallengeService challengeService;
